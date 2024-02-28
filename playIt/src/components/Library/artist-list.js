0bb314e8da641d6c0be0844b1729699e4ee04906
joshirakesh165 <email>
2024-02-28 22:29:22 +0530

    commitMessage

---
+++ playIt/src/components/Library/artist-list.js
@@ -0,0 +1,192 @@
+export const top_artists = [
+        {
+            "artistId": "459320",
+            "name": "Arijit Singh",
+            "image": "http://c.saavncdn.com/artists/Arijit_Singh_002_20230323062147_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/arijit-singh-songs/LlRWpHzy3Hk_"
+        },
+        {
+            "artistid": "464932",
+            "name": "Neha Kakkar",
+            "image": "http://c.saavncdn.com/artists/Neha_Kakkar_006_20200822042626_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/neha-kakkar-songs/EkEBV7JAU-I_"
+        },
+        {
+            "artistid": "568565",
+            "name": "Justin Bieber",
+            "image": "http://c.saavncdn.com/artists/Justin_Bieber_005_20201127112218_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/justin-bieber-songs/deJJWFd1ItE_"
+        },
+        {
+            "artistid": "459633",
+            "name": "Atif Aslam",
+            "image": "http://c.saavncdn.com/artists/Atif_Aslam_150x150.jpg",
+            "follower_count": 4760681,
+            "is_followed": false,
+            "permaUrl": "https://www.jiosaavn.com/artist/atif-aslam-songs/MXn8bhT308U_"
+        },
+        {
+            "artistid": "468245",
+            "name": "Diljit Dosanjh",
+            "image": "http://c.saavncdn.com/artists/Diljit_Dosanjh_005_20231025073054_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/diljit-dosanjh-songs/oIVHdWIO5F8_"
+        },
+        {
+            "artistid": "485956",
+            "name": "Yo Yo Honey Singh",
+            "image": "http://c.saavncdn.com/artists/Yo_Yo_Honey_Singh_002_20221216102650_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/yo-yo-honey-singh-songs/06QxyAvVpB4_"
+        },
+        {
+            "artistid": "456863",
+            "name": "Badshah",
+            "image": "http://c.saavncdn.com/artists/Badshah_005_20230608084021_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/badshah-songs/d4OwAaEcnD0_"
+        },
+        {
+            "artistid": "510237",
+            "name": "Nucleya",
+            "image": "http://c.saavncdn.com/artists/Nucleya_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/nucleya-songs/nZkjTeNYKxY_"
+        },
+        {
+            "artistid": "455125",
+            "name": "Sonu Nigam",
+            "image": "http://c.saavncdn.com/artists/Sonu_Nigam_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/sonu-nigam-songs/,kuQK6K6u0I_"
+        },
+        {
+            "artistid": "455109",
+            "name": "Lata Mangeshkar",
+            "image": "http://c.saavncdn.com/artists/Lata_Mangeshkar_004_20230623105323_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/lata-mangeshkar-songs/FCtl69DObYg_"
+        },
+        {
+            "artistid": "578407",
+            "name": "Ed Sheeran",
+            "image": "http://c.saavncdn.com/artists/Ed_Sheeran_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/ed-sheeran-songs/bWIDsVrU6DE_"
+        },
+        {
+            "artistid": "455130",
+            "name": "Shreya Ghoshal",
+            "image": "http://c.saavncdn.com/artists/Shreya_Ghoshal_004_20230228102304_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/shreya-ghoshal-songs/lIHlwHaxTZ0_"
+        },
+        {
+            "artistid": "674886",
+            "name": "Sanam Puri",
+            "image": "http://c.saavncdn.com/artists/Sanam_Puri_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/sanam-puri-songs/SkNEv3qRhDE_"
+        },
+        {
+            "artistid": "458918",
+            "name": "Raftaar",
+            "image": "http://c.saavncdn.com/artists/Raftaar_009_20230223100912_150x150.jpg",
+            "follower_count": 1276269,
+            "is_followed": false,
+            "permaUrl": "https://www.jiosaavn.com/artist/raftaar-songs/ZYd,kDiZhJg_"
+        },
+        {
+            "artistid": "455132",
+            "name": "Himesh Reshammiya",
+            "image": "http://c.saavncdn.com/artists/Himesh_Reshammiya_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/himesh-reshammiya-songs/N,m6H0-rqiY_"
+        },
+        {
+            "artistid": "464656",
+            "name": "Armaan Malik",
+            "image": "http://c.saavncdn.com/artists/Armaan_Malik_004_20231026182653_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/armaan-malik-songs/1iZ7Gi0bi1Y_"
+        },
+        {
+            "artistid": "461361",
+            "name": "Shah Rukh Khan",
+            "image": "http://c.saavncdn.com/artists/Shah_Rukh_Khan_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/shah-rukh-khan-songs/tkXMVGTn-A0_"
+        },
+        {
+            "artistid": "457931",
+            "name": "Deepika Padukone",
+            "image": "http://c.saavncdn.com/artists/Deepika_Padukone_002_20220131100114_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/deepika-padukone-songs/36LfdYhYGvc_"
+        },
+        {
+            "artistid": "459696",
+            "name": "Salman Khan",
+            "image": "http://c.saavncdn.com/artists/Salman_Khan_002_20200519100153_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/salman-khan-songs/uuAKJcbNOKg_"
+        },
+        {
+            "artistid": "511656",
+            "name": "Varun Dhawan",
+            "image": "http://c.saavncdn.com/artists/Varun_Dhawan_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/varun-dhawan-songs/nPLp3EdUdaA_"
+        },
+        {
+            "artistid": "511658",
+            "name": "Alia Bhatt",
+            "image": "http://c.saavncdn.com/artists/Alia_Bhatt_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/alia-bhatt-songs/,henzzVDXDQ_"
+        },
+        {
+            "artistid": "459632",
+            "name": "Ranbir Kapoor",
+            "image": "http://c.saavncdn.com/artists/Ranbir_Kapoor_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/ranbir-kapoor-songs/nqAfNmNB5II_"
+        },
+        {
+            "artistid": "462795",
+            "name": "Anushka Sharma",
+            "image": "http://c.saavncdn.com/artists/Anushka_Sharma_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/anushka-sharma-songs/kAK8j9P3jU8_"
+        },
+        {
+            "artistid": "455778",
+            "name": "Shahid Kapoor",
+            "image": "http://c.saavncdn.com/artists/Shahid_Kapoor_003_20230608090033_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/shahid-kapoor-songs/Ai6NXV-,6xI_"
+        },
+        {
+            "artistid": "531934",
+            "name": "Sunny Leone",
+            "image": "http://c.saavncdn.com/artists/Sunny_Leone_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/sunny-leone-songs/TDf7I3RaiT0_"
+        },
+        {
+            "artistid": "458135",
+            "name": "Allu Arjun",
+            "image": "http://c.saavncdn.com/artists/Allu_Arjun_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/allu-arjun-songs/BGi8EcKdZXk_"
+        },
+        {
+            "artistid": "457969",
+            "name": "Rajinikanth",
+            "image": "http://c.saavncdn.com/artists/Rajinikanth_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/rajinikanth-songs/E-hZmoCP5VU_"
+        },
+        {
+            "artistid": "456117",
+            "name": "Amitabh Bachchan",
+            "image": "http://c.saavncdn.com/artists/Amitabh_Bachchan_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/amitabh-bachchan-songs/X6Dce0iJ7y0_"
+        },
+        {
+            "artistid": "455785",
+            "name": "Priyanka Chopra",
+            "image": "http://c.saavncdn.com/artists/Priyanka_Chopra_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/priyanka-chopra-songs/ZOsjqZwNagU_"
+        },
+        {
+            "artistid": "459631",
+            "name": "Katrina Kaif",
+            "image": "http://c.saavncdn.com/artists/Katrina_Kaif_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/katrina-kaif-songs/GzitOgpaAM0_"
+        },
+        {
+            "artistid": "456196",
+            "name": "Vijay",
+            "image": "http://c.saavncdn.com/artists/Vijay_002_20191129094022_150x150.jpg",
+            "permaUrl": "https://www.jiosaavn.com/artist/vijay-songs/Poyjz0rbAbA_"
+        }
+    ]
