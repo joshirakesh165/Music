e19bbba99596ae8dc955c4c93850cc6bb18f6a94
joshirakesh165 <testing@gmail.com>
2024-02-29 18:46:38 +0530

    Code pushed from Api

---
+++ src/components/SideBar/Setting.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Cog8ToothIcon } from '@heroicons/react/24/solid'
+
+function Setting() {
+    return (
+        <div className='cursor-pointer rounded-md mb-24 ml-2 space-y-12 flex 
+         flex-col justify-center items-center w-12 h-12 text-white '>
+            <Cog8ToothIcon className='w-12' />
+        </div>
+    )
+}
+
+export default Setting
