f2e05b3efd75478a17559a527cb1fc055d3a81ce
joshirakesh165 <testing@gmail.com>
2024-02-29 18:46:33 +0530

    Code pushed from Api

---
+++ src/components/Player/PlayerContainer.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import Equalizer from './Equalizer'
+
+
+function PlayerContainer() {
+
+    return (
+        <Equalizer />
+    )
+}
+
+export default PlayerContainer
